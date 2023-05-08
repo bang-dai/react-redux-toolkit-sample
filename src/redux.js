58cc408c8d96b38fe2a689acea1b041decca2aaf
Bang DAI <bang.dai2@gmail.com>
2023-05-08 20:16:18 +0200

    first example with reduxjs/toolkit

---
+++ src/redux.js
@@ -0,0 +1,39 @@
+import { configureStore, createSlice } from "@reduxjs/toolkit";
+
+const todoSlice = createSlice({
+    name: "todo",
+    initialState: [
+        { id: 1, text: "Faire les courses", done: false },
+        { id: 2, text: "Ménage !", done: true },
+    ],
+    reducers: {
+        addTask: (state, action) => {
+            // {type: "todo/addTask", payload: 'Faire les courses'}
+            const newTask = {
+                id: Date.now(),
+                done: false,
+                text: action.payload
+            }
+
+            state.push(newTask)
+        },
+        toggleTask: (state, action) => {
+            // {type: "todo/toggleTask", payload: 20}
+            const task = state.find(t => t.id === action.payload)
+            task.done = !task.done
+        },
+        deleteTask: (state, action) => {
+            // {type: "todo/deleteTask", payload: 20}
+            state = state.filter(t => t.id !== action.payload)
+            return state
+        }
+    }
+})
+
+export const store = configureStore({
+    reducer: {
+        todo: todoSlice.reducer
+    }
+})
+
+export const { addTask, toggleTask, deleteTask } = todoSlice.actions
